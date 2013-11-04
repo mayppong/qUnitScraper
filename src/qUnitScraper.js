709dd04d8395d7c6de39f653cec9e01717c24edf
MayP <mayppong@gmail.com>
2013-11-03 16:25:11 -0800

    Add comments and reformat coding style
    Add comments to each method. It's not particular detailed but it should
    explain what the method does.
    Also add empty space inside some brackets to help make parameters
    easier to read.

--- src/qUnitScraper.js
+++ src/qUnitScraper.js
@@ -1,10 +1,29 @@
+/**
+ * qUnitScraper object
+ */
 var qUnitScraper = {
+    /**
+     * Start here!
+     * This is the first method to invoke to call other method. 
+     * Currently it can only does the call to other methods and doesn't
+     * do any processing here. 
+     */
     init: function() {
         // validate page here
         
         var failedMessages = this._readResults();
         console.log( failedMessages );
     },
+    /**
+     * _readResults create a jQuery object, selecting output listing. 
+     * Currently it's looking only for the failed modules, but could be 
+     * extended to specify which to look for.
+     * It calls other methods to get the information it needs and returns an object
+     * listing each method indexed by the method number as displayed to user.
+     * 
+     * @params : none
+     * @return : (object) listing all the failed modules along with their information.
+     */
     _readResults: function( ) {
         var failedModules = jQuery("#qunit-tests .fail[id^=qunit-test-output]");
         var failedMessages = [];
@@ -18,6 +37,16 @@ var qUnitScraper = {
         
         return failedMessages;
     },
+    /**
+     * This method takes jQuery object of the modules we want to read. 
+     * It then searches for the assert list result, currently the failed ones.
+     * Like _readResults, it could be extended.
+     * The method puts together the list found, builds an object including the name, message and source
+     * and returns it.
+     * 
+     * @params : (object) jQuery object of the module we want to read
+     * @return : (object) an object with 2 properties, name of test and another object listing the tests
+     */
     _readModule: function( failedModule ) {
         var failedTests   = jQuery(".qunit-assert-list .fail", failedModule);
         var failedMessages = [];
@@ -38,16 +67,16 @@ var qUnitScraper = {
         return failedMessages;
     },
     /**
-     * This one take a jQuery object of the module we want to find the number.
+     * This one takes a jQuery object of the module we want to find the number.
      * Performs a regex match over the id of the DOM which includes an index at the very end.
      * Lastly, we add 1 to the number since the ID is 0-indexed vs the list display to user is 1-indexed.
      * 
      * @params : (object) jQuery object of the module element
      * @return : (int) module number as display to users
      */
-    _getModuleNumber: function(module) {
-        var id = module.attr("id");
-        var match = id.match(/\d+$/)[0];
+    _getModuleNumber: function( module ) {
+        var id = module.attr( "id" );
+        var match = id.match( /\d+$/ )[0];
         return parseInt( match ) + 1 ;
     }
 }
